122fbc9465864ccd7b21b75f0ec1aa0f5db756e0
Matias Miranda <mati8765@gmail.com>
2020-12-10 21:21:37 -0300

    mapa configurado (2)

--- app/src/main/java/fi/unju/farmajuy/MapaFarmaciaActivity.java
+++ app/src/main/java/fi/unju/farmajuy/MapaFarmaciaActivity.java
@@ -2,6 +2,7 @@ package fi.unju.farmajuy;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,10 +11,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import fi.unju.farmajuy.utilidades.UtilidadesMapa;
 
 public class MapaFarmaciaActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    Double latInicial,longInicial,latFinal,longFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +48,57 @@ public class MapaFarmaciaActivity extends FragmentActivity implements OnMapReady
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        LatLng center = null;
+        ArrayList<LatLng> points = null;
+        PolylineOptions lineOptions = null;
+
+        // setUpMapIfNeeded();
+
+        // recorriendo todas las rutas
+        for(int i = 0; i< UtilidadesMapa.routes.size(); i++){
+            points = new ArrayList<LatLng>();
+            lineOptions = new PolylineOptions();
+
+            // Obteniendo el detalle de la ruta
+            List<HashMap<String, String>> path = UtilidadesMapa.routes.get(i);
+
+            // Obteniendo todos los puntos y/o coordenadas de la ruta
+            for(int j=0;j<path.size();j++){
+                HashMap<String,String> point = path.get(j);
+
+                double lat = Double.parseDouble(point.get("lat"));
+                double lng = Double.parseDouble(point.get("lng"));
+                LatLng position = new LatLng(lat, lng);
+
+                if (center == null) {
+                    //Obtengo la 1ra coordenada para centrar el mapa en la misma.
+                    center = new LatLng(lat, lng);
+                }
+                points.add(position);
+            }
+
+            // Agregamos todos los puntos en la ruta al objeto LineOptions
+            lineOptions.addAll(points);
+            //Definimos el grosor de las Polilíneas
+            lineOptions.width(2);
+            //Definimos el color de la Polilíneas
+            lineOptions.color(Color.BLUE);
+        }
+
+        // Dibujamos las Polilineas en el Google Map para cada ruta
+        mMap.addPolyline(lineOptions);
+
+        LatLng origen = new LatLng(UtilidadesMapa.coordenadas.getLatitudInicial(), UtilidadesMapa.coordenadas.getLongitudInicial());
+        mMap.addMarker(new MarkerOptions().position(origen).title("Lat: "+UtilidadesMapa.coordenadas.getLatitudInicial()+" - Long: "+UtilidadesMapa.coordenadas.getLongitudInicial()));
+
+        LatLng destino = new LatLng(UtilidadesMapa.coordenadas.getLatitudFinal(), UtilidadesMapa.coordenadas.getLongitudFinal());
+        mMap.addMarker(new MarkerOptions().position(destino).title("Lat: "+UtilidadesMapa.coordenadas.getLatitudFinal()+" - Long: "+UtilidadesMapa.coordenadas.getLongitudFinal()));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(center, 15));
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
---
+++ app/src/main/java/fi/unju/farmajuy/utilidades/UtilidadesMapa.java
@@ -0,0 +1,10 @@
+package fi.unju.farmajuy.utilidades;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UtilidadesMapa {
+    public static List<List<HashMap<String, String>>> routes = new ArrayList<List<HashMap<String,String>>>();
+    public static Punto coordenadas=new Punto();
+}
